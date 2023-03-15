b0a05f44e2a02166ee9f6ca267f2f16991f6816a
sumithanvat <skhanvat@gmail.com>
2023-03-15 23:31:46 +0530

    Added:Wellcome to Stock Account Management

---
+++ src/com/bridgelabz/StockAccountManagement.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class StockAccountManagement {
+    public static void main(String[] args) {
+        System.out.println("wellcome to stock account management");}
+}
